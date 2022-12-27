45effebff5f09d51e81a7db29109202d70a6edd5
jainl28patel <jainilpatel2003@gmail.com>
2022-12-27 16:27:27 +0530

    Added spotlight with smooth borders

--- Shaders/fragment.fs
+++ Shaders/fragment.fs
@@ -15,6 +15,9 @@ struct Light {
 
     vec3 direction;
     vec3 position;
+    float cuttoff;
+    float outerCuttoff;
+
     vec3 ambient;
     vec3 diffuse;
     vec3 specular;
@@ -35,8 +38,7 @@ void main()
 
     // diffuse
     vec3 norm = normalize(Normal);
-//     vec3 lightDir = normalize(light.position - FragPos);
-    vec3 lightDir = normalize(-light.direction);
+    vec3 lightDir = normalize(light.position - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexCoord).rgb;
 
@@ -46,10 +48,20 @@ void main()
     float spec = pow(max(dot(view_direction, reflect_direction), 0.0f), material.shininess);
     vec3 specular = light.specular * spec * texture(material.specular, TexCoord).rgb;
 
+    // SpotLight with Soft Edges
+    float theta     = dot(lightDir, normalize(-light.direction));
+    float epsilon   = light.cuttoff - light.outerCuttoff;
+    float intensity = clamp((theta - light.outerCuttoff) / epsilon, 0.0, 1.0);
+    specular *= intensity;
+    diffuse *= intensity;
+
     // attenuation
     float distance    = length(light.position - FragPos);
     float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
+    ambient  *= attenuation;
+    diffuse   *= attenuation;
+    specular *= attenuation;
 
-    vec3 result = (ambient + diffuse + specular) * attenuation;
+    vec3 result = ambient + diffuse + specular;
     FragColor = vec4(result, 1.0);
 }
--- main.cpp
+++ main.cpp
@@ -30,7 +30,7 @@ float lastFrame = 0.0f;
 
 // lighting
 //glm::vec3 lightPos(10.0f, 0.0f, 0.0f);
-glm::vec3 lightPos(0.8f, 0.8f, 0.8f);
+glm::vec3 lightPos(0.0f, 0.0f, 5.0f);
 
 int main()
 {
@@ -141,13 +141,17 @@ int main()
 
         // be sure to activate shader when setting uniforms/drawing objects
         lightingShader.use();
-        lightingShader.setVec3("light.position", lightPos);
-        lightingShader.setVec3("light.direction", -0.2f, -1.0f, -0.3f);
+//        lightingShader.setVec3("light.position", lightPos);
+//        lightingShader.setVec3("light.direction", -0.0f, -0.0f, -0.1f);
+        lightingShader.setVec3("light.position", camera.Position);
+        lightingShader.setVec3("light.direction", camera.Front);
+        lightingShader.setFloat("light.cuttoff", glm::cos(glm::radians(12.5f)));
+        lightingShader.setFloat("light.outerCutOff", glm::cos(glm::radians(17.6f)));
         lightingShader.setVec3("cameraPos", camera.Position);
 
         // light properties
-        lightingShader.setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
-        lightingShader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
+        lightingShader.setVec3("light.ambient", 0.1f, 0.1f, 0.1f);
+        lightingShader.setVec3("light.diffuse", 0.8f, 0.8f, 0.8f);
         lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
         lightingShader.setFloat("light.constant", 1.0f);
         lightingShader.setFloat("light.linear", 0.09f);
@@ -155,8 +159,7 @@ int main()
 
 
         // material properties
-        lightingShader.setVec3("material.specular", 0.5f, 0.5f, 0.5f);
-        lightingShader.setFloat("material.shininess", 64.0f);
+        lightingShader.setFloat("material.shininess", 32.0f);
 
 
         // view/projection transformations
@@ -186,17 +189,17 @@ int main()
 
 
         // also draw the lamp object
-        lightCubeShader.use();
-        lightCubeShader.setVec3("color", 1.0f, 1.0f, 1.0f);
-        lightCubeShader.setMat4("projection", projection);
-        lightCubeShader.setMat4("view", view);
-        model = glm::mat4(1.0f);
-        model = glm::translate(model, lightPos);
-        model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
-        lightCubeShader.setMat4("model", model);
-
-        glBindVertexArray(lightCubeVAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+//        lightCubeShader.use();
+//        lightCubeShader.setVec3("color", 1.0f, 1.0f, 1.0f);
+//        lightCubeShader.setMat4("projection", projection);
+//        lightCubeShader.setMat4("view", view);
+//        model = glm::mat4(1.0f);
+//        model = glm::translate(model, lightPos);
+//        model = glm::scale(model, glm::vec3(0.2f)); // a smaller cube
+//        lightCubeShader.setMat4("model", model);
+//
+//        glBindVertexArray(lightCubeVAO);
+//        glDrawArrays(GL_TRIANGLES, 0, 36);
 
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
